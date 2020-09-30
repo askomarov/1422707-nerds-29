58304c4863753c7ad215e89019ca933bb2df5f5d
askomarov <askomarov13@mail.ru>
2020-09-30 23:58:58 +0300

    правки после проверки + правки по бэму

--- js/scrtipt.js
+++ js/scrtipt.js
@@ -1,19 +1,19 @@
 
 
-var html5Slider = document.getElementById('slider');
+var html5Slider = document.getElementById("slider");
 
 noUiSlider.create(html5Slider, {
   start: [10, 15000],
   connect: true,
   range: {
-    'min': 0,
-    'max': 20000
+    "min": 0,
+    "max": 20000
   }
 });
 
-var inputNumber = document.getElementById('input-number');
-var inputNumber2 = document.getElementById('input-number-2');
-html5Slider.noUiSlider.on('update', function (values, handle) {
+var inputNumber = document.getElementById("input-number");
+var inputNumber2 = document.getElementById("input-number-2");
+html5Slider.noUiSlider.on("update", function (values, handle) {
 
   var value = values[handle];
 
@@ -23,9 +23,9 @@ html5Slider.noUiSlider.on('update', function (values, handle) {
     inputNumber2.value = Math.round(value);
   }
 });
-inputNumber.addEventListener('change', function () {
+inputNumber.addEventListener("change", function () {
   html5Slider.noUiSlider.set([null, this.value]);
 });
-inputNumber2.addEventListener('change', function () {
+inputNumber2.addEventListener("change", function () {
   html5Slider.noUiSlider.set([null, this.value]);
 });
